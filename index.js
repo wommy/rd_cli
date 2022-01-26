ccc2d9a4524c6375609f3d4b6529d095e3d9b01d
wommy <tom@technomad.media>
2022-01-25 22:21:08 -0500

    pushit

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 #!/usr/bin/env node
-console.log(`
+import chalk from 'chalk'
+const log = console.log
+const dimgray = chalk.dim.gray
+const dg = dimgray
+
+const todo = `- [ ] `
+const mapTodo = x => x.map( y => todo+y ).join('\n')
+const future = [
+	`write RD-tudo`
+]
+const now = [
+	`get the date`,
+	`write to new file`,
+	`todo --new`,
+	`rd --new`,
+	`rd __ => cd /rd && rd --new`,
+]
+
+console.clear()
+log(dg(`
+${
+process.argv.slice(2).indexOf('--no-future') === -1 
+	? mapTodo(future)
+	: ``
+}
+
 TODO:
-- [ ] write RD-tudo
+${mapTodo(now)}
 
-
-what should i write here
-
-i need to get the date 
-and write to a new file
-
-
-
-`)
+`))
+log(`args:`, process.argv.slice(2))
