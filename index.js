2096aaea1ff4c0ea911bb699396084ee30e7ecfd
wommy <tom@technomad.media>
2022-01-27 16:48:52 -0500

    feat: lowdb

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 #!/usr/bin/env node
-import chalk from 'chalk'
 const log = console.log
+import chalk from 'chalk'
 const dimgray = chalk.dim.gray
 const dg = dimgray
 
-const todo = `- [ ] `
-const mapTodo = x => x.map( y => todo+y ).join('\n')
-const future = [
-	`write RD-tudo`
-]
-const now = [
-	`get the date`,
-	`write to new file`,
-	`todo --new`,
-	`rd --new`,
-	`rd __ => cd /rd && rd --new`,
-]
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
+import { Low, JSONFile } from 'lowdb'
+const Tudos = new Low( new JSONFile( join( dirname(fileURLToPath(import.meta.url)), 'Tudos.json') ) )
+await Tudos.read()
+// Tudos.data ||= {} // INIT
+// Tudos.data.push( CATEGORY: [] ) // new Category
+const { future, now } = Tudos.data
+// new Tudo
+// now.push( 'api.github repos' )
+// await tudos.write()
+
+const mapTodo = x => x.map( y => `- [ ] `+y ).join('\n')
+// const future = [
+// 	`write RD-tudo`
+// ]
+// const now = [
+// 	`get the date`,
+// 	`write to new file`,
+// 	`todo --new`,
+// 	`rd --new`,
+// 	`rd __ => cd /rd && rd --new`,
+// 	`api.github repos`,
+// ]
 
 console.clear()
 log(dg(`
